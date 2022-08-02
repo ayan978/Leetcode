3fe2bef4c25a01af78c3f90621b2cbbed0397256
unknown <sadiqunnur.97@gmail.com>
2022-08-02 15:49:56 +0600

    Commit

---
+++ Roman.py
@@ -0,0 +1,63 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        num = 0
+
+        for i in range(len(s)):
+            if s[i] == 'I':
+                if s[i + 1] == 'V':
+                    num += 4
+                    i += 1
+                    continue
+                elif s[i + 1] == 'X':
+                    num += 9
+                    i += 1
+                    continue
+                else:
+                    num += 1
+
+            elif s[i] == 'X':
+                if s[i + 1] == 'L':
+                    num += 40
+                    i += 1
+                    continue
+                elif s[i + 1] == 'C':
+                    num += 90
+                    i += 1
+                    continue
+                else:
+                    num += 10
+
+            elif s[i] == 'C':
+                if s[i + 1] == 'D':
+                    num += 400
+                    i += 1
+                    continue
+                elif s[i + 1] == 'M':
+                    num += 900
+                    i += 1
+                    continue
+                else:
+                    num += 100
+
+            else:
+                if s[i] == 'V':
+                    num += 5
+                elif s[i] == 'L':
+                    num += 50
+                elif s[i] == 'C':
+                    num += 100
+                elif s[i] == 'D':
+                    num += 500
+                else:
+                    num += 1000
+
+        return num
+
+
+
+s1 = Solution()
+print(s1.romanToInt('III'))
