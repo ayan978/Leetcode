e18bde43abadd5e3e2771d50321ccbc32c508e05
unknown <sadiqunnur.97@gmail.com>
2022-08-03 14:17:29 +0600

    9. Palindrome Number Solved

---
+++ 9. Palindrome Number.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        x1 = str(x)
+        i = 0
+        j = len(x1) - 1
+        flag = True
+        while i < len(x1):
+            if x1[i] == x1[j]:
+                i += 1
+                j -= 1
+
+            else:
+                flag = False
+                break
+
+        return flag
