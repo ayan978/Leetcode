f4e637ba453065d784c960b5c07fe636af3eaadc
Sadiqun Nur Ayan <65063367+ayan978@users.noreply.github.com>
2025-09-28 23:28:46 +1000

    Update 197. Rising Temperature.sql

--- 197. Rising Temperature.sql
+++ 197. Rising Temperature.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
 
-SELECT w2.id AS id FROM Weather w1 JOIN Weather w2 WHERE w1.id = w2.id - 1 AND w2.temperature > w1.temperature;
+SELECT w2.id AS id FROM Weather w1 JOIN Weather w2 WHERE w2.recordDate = w1.recordDate + INTERVAL 1 DAY AND w2.temperature > w1.temperature;
